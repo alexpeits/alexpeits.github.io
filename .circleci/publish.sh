94a501d30cc7450fa53e0eaaa6ceb055c06c65b0
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2018-09-27 16:20:26 +0100

    That's it

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -6,7 +6,7 @@ git config user.email "$USER_EMAIL"
 git checkout master
 git pull origin master
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
