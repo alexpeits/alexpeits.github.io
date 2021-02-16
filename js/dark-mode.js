7ae8e9086509140aba9a4d537d1526c7e3e3ea65
alexpeits <alexpeitsinis@gmail.com>
2021-02-16 20:12:16 +0000

    Deploying to master from  @ 3eea25dc4ea21a18bce021d7b5d985a3b2658807 🚀

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,7 +1,14 @@
-clsWhenLight="dark-mode-disabled";clsWhenDark="dark-mode-enabled";btnId="dark-mode-toggle";faIcon="fa-moon";storeKey="dark-mode";function isDarkMode(){darkMode=sessionStorage.getItem(storeKey);return darkMode==="true";}
-function setDarkMode(darkMode){if(darkMode){document.documentElement.setAttribute("data-theme","dark");sessionStorage.setItem(storeKey,"true");}else{document.documentElement.removeAttribute("data-theme");sessionStorage.removeItem(storeKey);}}
-function setDarkModeIcon(darkMode){btn=document.getElementById(btnId);if(darkMode){btn.classList.remove(clsWhenLight);btn.classList.add(clsWhenDark);}else{btn.classList.remove(clsWhenDark);btn.classList.add(clsWhenLight);}}
-function toggleDarkMode(){darkMode=isDarkMode();setDarkMode(!darkMode);setDarkModeIcon(!darkMode);}
-if(isDarkMode()){document.documentElement.setAttribute("data-theme","dark");}
+clsWhenLight="dark-mode-disabled";clsWhenDark="dark-mode-enabled";btnId="dark-mode-toggle";faIcon="fa-moon";storeKey="dark-mode";systemPrefersMatch="(prefers-color-scheme: dark)"
+function updateIcon(darkMode){btn=document.getElementById(btnId);if(darkMode){btn.classList.remove(clsWhenLight);btn.classList.add(clsWhenDark);}else{btn.classList.remove(clsWhenDark);btn.classList.add(clsWhenLight);}}
+function toggleDarkMode(){attr=document.documentElement.getAttribute("data-theme");darkMode=attr==="dark";if(darkMode){setLightMode();updateIcon(false);setUserPreference(false);}else{setDarkMode();updateIcon(true);setUserPreference(true);}}
+function setUserPreference(darkMode){pref=darkMode?"true":"false";sessionStorage.setItem(storeKey,pref)}
+function clearUserPreference(){sessionStorage.removeItem(storeKey)}
+function userPrefersDarkMode(){darkMode=sessionStorage.getItem(storeKey);return darkMode==="true";}
+function userPrefersLightMode(){darkMode=sessionStorage.getItem(storeKey);return darkMode==="false";}
+function setDarkMode(){document.documentElement.setAttribute("data-theme","dark");}
+function setLightMode(){document.documentElement.removeAttribute("data-theme");}
+function shouldDarkMode(){if(userPrefersDarkMode()){return true;}else if(userPrefersLightMode()){return false;}else if(window.matchMedia(systemPrefersMatch).matches){return true;}else{return false;}}
+if(shouldDarkMode()){setDarkMode();}else{setLightMode();}
 window.onload=function(){btn=document.getElementById(btnId);btn.classList.remove("dark-mode-disabled")
-setDarkModeIcon(isDarkMode());}
+if(shouldDarkMode()){updateIcon(true);}else{updateIcon(false);}
+window.matchMedia(systemPrefersMatch).addEventListener("change",event=>{if(event.matches){clearUserPreference();setDarkMode();updateIcon(true);}else{clearUserPreference();setLightMode();updateIcon(false);}});}
