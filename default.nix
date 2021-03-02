683f362f52eb7ae6b6e7871f787c73e9a6d85369
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2021-03-02 00:04:50 +0000

    css and font fixes

--- default.nix
+++ default.nix
@@ -2,11 +2,11 @@
 let
   nixpkgs =
     if isNull pkgs then
-      import (import ./nix/sources.nix).nixpkgs {}
+      import (import ./nix/sources.nix).nixpkgs { }
     else if builtins.typeOf pkgs == "set" then
       pkgs
     else
-      import (builtins.getAttr pkgs (import ./nix/sources.nix)) {};
+      import (builtins.getAttr pkgs (import ./nix/sources.nix)) { };
 
   haskellPackagesBase =
     if isNull compiler then
@@ -37,9 +37,9 @@ let
           super = super;
         };
         src = nixpkgs.lib.sourceByRegex ./. appSrcRegex;
-        drv = self.callCabal2nix "peits" src {};
+        drv = self.callCabal2nix "peits" src { };
       in
-        hsPkgs // { peits = drv; };
+      hsPkgs // { peits = drv; };
   };
 
   yarn = nixpkgs.yarn.override { nodejs = nixpkgs.nodejs-12_x; };
@@ -71,7 +71,7 @@ let
       (arch: builtins.currentSystem == arch)
       [ "x86_64-darwin" ];
 
-  inotify = if isDarwin then [] else [ nixpkgs.inotify-tools ];
+  inotify = if isDarwin then [ nixpkgs.entr ] else [ nixpkgs.inotify-tools ];
 
   shell = haskellPackages.shellFor {
     packages = ps: [ ps.peits ];
